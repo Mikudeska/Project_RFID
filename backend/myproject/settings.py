fec780fbced8e86840fc92fe1521599b209b9dc5
mikudeska <mikudeska@gmail.com>
2025-05-19 13:01:03 +0700

    setting env

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,11 +88,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ssru_student_db',  # ชื่อฐานข้อมูลที่สร้างใน phpMyAdmin
-        'USER': 'root',        # ชื่อผู้ใช้ของ MySQL ใน XAMPP คือ 'root' โดยปกติ
-        'PASSWORD': '',        # รหัสผ่านของ MySQL ใน XAMPP โดยปกติจะเป็นค่าว่าง
-        'HOST': 'localhost',   # ตั้งค่าเป็น localhost
-        'PORT': '3306',        # ค่าเริ่มต้นของพอร์ต MySQL
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='3306'),
     }
 }
 
